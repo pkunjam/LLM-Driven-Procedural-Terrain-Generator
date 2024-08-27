45c0d4e79b631422a9997bd8af8fce58434b8246
pkunjam <kunjampunit@gmail.com>
2024-08-27 19:39:43 -0400

    added textures

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,10 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-#include "PerlinNoise.cpp"   
-#include "ArcballCamera.cpp" 
+#include "PerlinNoise.cpp"
+#include "ArcballCamera.cpp"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 // Global Variables
 ArcballCamera camera(glm::vec3(0.0f, 0.0f, 0.0f), 5.0f, -90.0f, 20.0f);
@@ -21,9 +23,11 @@ void processInput(GLFWwindow *window);
 void mouse_callback(GLFWwindow *window, double xpos, double ypos);
 void mouse_button_callback(GLFWwindow *window, int button, int action, int mods);
 void scroll_callback(GLFWwindow *window, double xOffset, double yOffset);
-void generateGrid(int width, int height, std::vector<float> &vertices, std::vector<unsigned int> &indices, int octaves, float persistence);
+void generateAdvancedTerrain(int width, int height, std::vector<float> &vertices, std::vector<unsigned int> &indices);
 unsigned int createShaderProgram();
+unsigned int loadTexture(const char *path);
 void setupBuffers(unsigned int &VAO, unsigned int &VBO, unsigned int &EBO, const std::vector<float> &vertices, const std::vector<unsigned int> &indices);
+void checkOpenGLError();
 
 // Main Function
 int main()
@@ -68,13 +72,24 @@ int main()
     // Configure Global OpenGL State
     glEnable(GL_DEPTH_TEST);
 
-    // Generate Terrain Grid
+    // Load Textures
+    unsigned int grassTexture = loadTexture("textures/grass.png");
+    unsigned int rockTexture = loadTexture("textures/rock.png");
+    unsigned int snowTexture = loadTexture("textures/snow.jpg");
+
+    // Generate Advanced Terrain Grid
     std::vector<float> vertices;
     std::vector<unsigned int> indices;
-    generateGrid(100, 100, vertices, indices, 5, 0.5f);
+    generateAdvancedTerrain(100, 100, vertices, indices);
 
     // Create Shader Program
     unsigned int shaderProgram = createShaderProgram();
+
+    // Set texture uniforms
+    glUseProgram(shaderProgram);
+    glUniform1i(glGetUniformLocation(shaderProgram, "grassTexture"), 0);
+    glUniform1i(glGetUniformLocation(shaderProgram, "rockTexture"), 1);
+    glUniform1i(glGetUniformLocation(shaderProgram, "snowTexture"), 2);
 
     // Setup Buffers
     unsigned int VAO, VBO, EBO;
@@ -91,6 +106,16 @@ int main()
 
         // Use Shader Program
         glUseProgram(shaderProgram);
+
+        // Bind Textures
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, grassTexture);
+
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, rockTexture);
+
+        glActiveTexture(GL_TEXTURE2);
+        glBindTexture(GL_TEXTURE_2D, snowTexture);
 
         // View/Projection Transformations
         glm::mat4 view = camera.GetViewMatrix();
@@ -109,6 +134,9 @@ int main()
         // Swap Buffers and Poll Events
         glfwSwapBuffers(window);
         glfwPollEvents();
+
+        // Check for OpenGL errors
+        checkOpenGLError();
     }
 
     // Cleanup
@@ -181,21 +209,41 @@ void scroll_callback(GLFWwindow *window, double xOffset, double yOffset)
     camera.ProcessMouseScroll(yOffset);
 }
 
-// Generate Terrain Grid
-void generateGrid(int width, int height, std::vector<float> &vertices, std::vector<unsigned int> &indices, int octaves, float persistence)
+// Generate Advanced Terrain with Multiple Layers of Perlin Noise
+void generateAdvancedTerrain(int width, int height, std::vector<float> &vertices, std::vector<unsigned int> &indices)
 {
     float scale = 1.0f / (std::max(width, height) - 1);
+
+    int numOctaves = 4;
+    float persistence = 0.5f;
+    float lacunarity = 2.0f;
+    float baseAmplitude = 0.5f;
+    float baseFrequency = 0.4f;
+
     for (int z = 0; z < height; ++z)
     {
         for (int x = 0; x < width; ++x)
         {
             float xPos = (x * scale) - 0.5f;
             float zPos = (z * scale) - 0.5f;
-            float yPos = perlin.noise(xPos, zPos, octaves, persistence);
+
+            float heightValue = 0.0f;
+            float amplitude = baseAmplitude;
+            float frequency = baseFrequency;
+
+            for (int octave = 0; octave < numOctaves; ++octave)
+            {
+                heightValue += amplitude * perlin.noise(xPos * frequency, zPos * frequency, numOctaves, persistence);
+                amplitude *= persistence;
+                frequency *= lacunarity;
+            }
 
             vertices.push_back(xPos);
-            vertices.push_back(yPos);
+            vertices.push_back(heightValue); // Height
             vertices.push_back(zPos);
+
+            vertices.push_back(static_cast<float>(x) / (width - 1));  // Texture X coordinate
+            vertices.push_back(static_cast<float>(z) / (height - 1)); // Texture Y coordinate
 
             if (x < width - 1 && z < height - 1)
             {
@@ -217,17 +265,40 @@ unsigned int createShaderProgram()
     const char *vertexShaderSource = R"glsl(
         #version 330 core
         layout(location = 0) in vec3 aPos;
+        layout(location = 1) in vec2 aTexCoord;
+        out vec2 TexCoords;
+        out float Height;
+
         uniform mat4 transform;
+
         void main() {
+            TexCoords = aTexCoord;
+            Height = aPos.y;
             gl_Position = transform * vec4(aPos, 1.0);
         }
     )glsl";
 
     const char *fragmentShaderSource = R"glsl(
         #version 330 core
+        in vec2 TexCoords;
+        in float Height;
         out vec4 FragColor;
+
+        uniform sampler2D grassTexture;
+        uniform sampler2D rockTexture;
+        uniform sampler2D snowTexture;
+
         void main() {
-            FragColor = vec4(0.53, 0.44, 0.26, 1.0);
+            vec4 grassColor = texture(grassTexture, TexCoords);
+            vec4 rockColor = texture(rockTexture, TexCoords);
+            vec4 snowColor = texture(snowTexture, TexCoords);
+
+            if (Height < 0.3)
+                FragColor = grassColor;
+            else if (Height < 0.6)
+                FragColor = mix(grassColor, rockColor, (Height - 0.3) / 0.3);
+            else
+                FragColor = mix(rockColor, snowColor, (Height - 0.6) / 0.4);
         }
     )glsl";
 
@@ -236,10 +307,30 @@ unsigned int createShaderProgram()
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
 
+    // Check for Vertex Shader compilation errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cerr << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+                  << infoLog << std::endl;
+    }
+
     // Compile Fragment Shader
     unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(fragmentShader);
+
+    // Check for Fragment Shader compilation errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cerr << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
+                  << infoLog << std::endl;
+    }
 
     // Link Shaders to Create the Shader Program
     unsigned int shaderProgram = glCreateProgram();
@@ -247,11 +338,59 @@ unsigned int createShaderProgram()
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
 
+    // Check for Linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cerr << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
+                  << infoLog << std::endl;
+    }
+
     // Clean up Shaders as they're now linked into the program
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
     return shaderProgram;
+}
+
+// Load Texture
+unsigned int loadTexture(const char *path)
+{
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+
+    int width, height, nrComponents;
+    unsigned char *data = stbi_load(path, &width, &height, &nrComponents, 0);
+    if (data)
+    {
+        GLenum format;
+        if (nrComponents == 1)
+            format = GL_RED;
+        else if (nrComponents == 3)
+            format = GL_RGB;
+        else if (nrComponents == 4)
+            format = GL_RGBA;
+
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        stbi_image_free(data);
+    }
+    else
+    {
+        std::cerr << "Texture failed to load at path: " << path << std::endl;
+        stbi_image_free(data);
+        return 0;
+    }
+
+    return textureID;
 }
 
 // Setup Buffers
@@ -269,9 +408,22 @@ void setupBuffers(unsigned int &VAO, unsigned int &VBO, unsigned int &EBO, const
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
+}
+
+// Check for OpenGL Errors
+void checkOpenGLError()
+{
+    GLenum err;
+    while ((err = glGetError()) != GL_NO_ERROR)
+    {
+        std::cerr << "OpenGL error: " << err << std::endl;
+    }
 }
